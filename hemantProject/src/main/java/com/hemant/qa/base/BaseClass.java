865b65dda2a0c30d0c75d17e41bf7cfb00650bc1
hzope12 <43350930+hzope12@users.noreply.github.com>
2018-09-29 00:14:49 +0530

    29_09_2018
    Added Firefox Launch Code

--- hemantProject/src/main/java/com/hemant/qa/base/BaseClass.java
+++ hemantProject/src/main/java/com/hemant/qa/base/BaseClass.java
@@ -1,5 +1,6 @@
 package com.hemant.qa.base;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -7,6 +8,9 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.hemant.slim.SlimClass;
@@ -24,8 +28,10 @@ public class BaseClass {
 		try 
 		{
 			prop = new Properties();
-//			FileInputStream input = new FileInputStream("com/configuration/config.properties");
-			FileInputStream input = new FileInputStream("F://Testing Stuff//Automation//SeicProject//com.configuration//config.properties");
+//			File f1 = new File("config.properties");
+//			String path = f1.getAbsolutePath();
+			FileInputStream input = new FileInputStream(".//com.configuration//config.properties");
+//			FileInputStream input = new FileInputStream("F://Testing Stuff//Automation//SeicProject//com.configuration//config.properties");
 			prop.load(input);			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -39,16 +45,39 @@ public class BaseClass {
 		String browser = prop.getProperty("browser");
 		if(browser.equalsIgnoreCase("chrome")) 
 		{
-			driver = new ChromeDriver();
-			sc = new SlimClass();
-			wait = new org.openqa.selenium.support.ui.WebDriverWait(driver, 180);
-			driver.manage().window().maximize();
-//			driver.manage().deleteAllCookies();
-			driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
-			driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUTS, TimeUnit.SECONDS);
-			driver.get(prop.getProperty("url"));
-			TestUtil.waitForPageLoad();
+			launchChromeBrowser();
+		}else if(browser.equalsIgnoreCase("firefox")) 
+		{
+			launchFirefoxBrowser();
 		}
+		
+		sc = new SlimClass();
+		wait = new org.openqa.selenium.support.ui.WebDriverWait(driver, 180);
+		driver.manage().window().maximize();
+//		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUTS, TimeUnit.SECONDS);
+		driver.get(prop.getProperty("url"));
+		TestUtil.waitForPageLoad();
 	}
 	
+	public void launchChromeBrowser() 
+	{
+		driver = new ChromeDriver();
+	}
+	
+	public void launchFirefoxBrowser() 
+	{
+		System.setProperty("webdriver.gecko.driver", ".//geckodriver.exe");
+//		DesiredCapabilities cap = new DesiredCapabilities().firefox();
+//		cap.setCapability("marionette", true);
+//		driver = new FirefoxDriver(cap);
+//		System.setProperty("webdriver.firefox.marionette", ".//geckodriver.exe");
+		driver = new FirefoxDriver();
+//		FirefoxOptions options = new FirefoxOptions();
+//		options.setLegacy(true);
+//		FirefoxOptions options = new FirefoxOptions();
+//		options.setLogLevel(FirefoxDriverLogLevel.DEBUG);
+//		driver = new FirefoxDriver(options);
+	}
 }
--- hemantProject/src/test/java/com/hemant/qa/testcases/LoginPageTest.java
+++ hemantProject/src/test/java/com/hemant/qa/testcases/LoginPageTest.java
@@ -24,7 +24,7 @@ public class LoginPageTest extends BaseClass{
 		
 		lp.navigateToSingIn();
 		lp.signIn();
-		Thread.sleep(10000);
+		Thread.sleep(2000);
 		
 	}
 	
